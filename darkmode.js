ec57ac89bceccea6fa94028a3ade1ff656f89bb4
mariimao <66910658+mariimao@users.noreply.github.com>
2024-07-25 17:27:30 -0400

    topbar testing

--- darkmode.js
+++ darkmode.js
@@ -7,9 +7,14 @@ function initializeDarkModeToggle() {
     const currentTheme = localStorage.getItem('theme');
     if (currentTheme) {
         htmlElement.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeColorMetaTag.setAttribute('content', '#262a67'); // Dark mode theme color
+        } else {
+            themeColorMetaTag.setAttribute('content', '#9a5752'); // Light mode theme color
+        }
     } else {
         htmlElement.classList.add('light-mode');
-        themeColorMetaTag.setAttribute('content', '#9a5752');
+        themeColorMetaTag.setAttribute('content', '#9a5752'); // Default to light mode theme color
     }
 
     darkToggle.addEventListener('click', function() {
@@ -17,12 +22,14 @@ function initializeDarkModeToggle() {
             htmlElement.classList.remove('light-mode');
             htmlElement.classList.add('dark-mode');
             localStorage.setItem('theme', 'dark-mode');
-            themeColorMetaTag.setAttribute('content', '#262a67');
-        } else { // switch to light mode
+            themeColorMetaTag.setAttribute('content', '#262a67'); // Dark mode theme color
+        } else { // Switch to light mode
             htmlElement.classList.remove('dark-mode');
             htmlElement.classList.add('light-mode');
             localStorage.setItem('theme', 'light-mode');
-            themeColorMetaTag.setAttribute('content', '#9a5752');
+            themeColorMetaTag.setAttribute('content', '#9a5752'); // Light mode theme color
         }
     });
 }
+
+initializeDarkModeToggle();
