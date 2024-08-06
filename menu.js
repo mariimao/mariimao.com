44c164c6078180bc5a518acdbf074d4ed7167c2c
mariimao <66910658+mariimao@users.noreply.github.com>
2024-08-06 09:52:48 -0400

    resizing background and mobile menu started

---
+++ menu.js
@@ -0,0 +1,17 @@
+// TODO
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleMenu() {
+        const menuToggle = document.getElementById('menuToggle');
+        const navbar = document.getElementById('navbar');
+
+        menuToggle.addEventListener('click', function() {
+            if (navbar.style.display === 'block') {
+                navbar.style.display = 'none';
+            } else {
+                
+        }
+    }
+}
+
+    toggleMenu();
+});
